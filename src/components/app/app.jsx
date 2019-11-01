e9fb7091df18aabc8d1a7b385f46dce9fdc202b1
Yana <ania.lanskaya@gmail.com>
2019-11-01 21:43:06 +0100

    Adding PropTypes & MovieCard

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Main from '../main/main.jsx';
 
-const App = () => {
+const App = (props) => {
+  const {filmTitles} = props;
   return (
-    <Main />
+    <Main filmTitles = {filmTitles} />
   );
+};
+
+App.propTypes = {
+  filmTitles: PropTypes.array,
 };
 
 export default App;
